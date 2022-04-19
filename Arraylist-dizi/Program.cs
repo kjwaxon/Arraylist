ae440fd6ae7a7aa2fbe47cbaadc8f37c9b712987
kjwaxon <kaanaslan1996@hotmail.com>
2022-04-19 14:23:13 +0300

    Arraylist

---
+++ Arraylist-dizi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace Arraylist_dizi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList kaç variable olucağını veya variable tipini tanımlamamıza gerek yok--
+            ArrayList x = new ArrayList();
+            x.Add(77);
+            x.Add("KAAN");
+            x.Add(3.14);
+            foreach (var item in x)
+                {
+                Console.WriteLine(item + ",");
+            }
+                        Console.ReadKey();
+        }
+
+
+    }
+}
